13f0fcccf8d4872058a611d3d7621310dd057769
Douwe Brinkhorst <douwe.brinkhorst@gmail.com>
2020-01-13 16:10:36 +0100

    Added neccesary loops, more elegant indexing and more comments

--- medianblur.cpp
+++ medianblur.cpp
@@ -25,16 +25,25 @@ void medianblur(pixel_stream &src, pixel_stream &dst)
 #pragma HLS INTERFACE s_axilite port=r
 #pragma HLS PIPELINE II=1
 
-const int k = 5;				//half-aperture of median blur
+// k determines the aperture size
+// this aperture is then a 2*k+1 by 2*k+1 grid
+const int k = 5;
 const int bufferheight = 2*k+1;
 
+// buffer that stores several lines required for blur computation
 static uint32_t buffer [WIDTH][bufferheight];
+// a temporary buffer that stores incoming data of the new line while the previous one is still being processed
 static uint32_t tempbuffer [k];
-static uint16_t i = 0;
-static uint16_t d = k;
+
+//index of where to store the incoming pixel
+static uint16_t storage_index = 0;
+// index of the next pixel to go to output
+static int16_t output_index = 0;
+
+uint16_t i = 0;
 uint16_t j = 1;
 uint16_t lowerX = 0;
-uint16_t upperX = 0;
+uint16_t upperX = WIDTH-1;
 
 pixel_data p_in;
 
@@ -42,34 +51,49 @@ pixel_data p_in;
 src >> p_in;
 static pixel_data p_out = p_in;
 
-if (d>0) {
-	tempbuffer[i] = p_in.data;
-
-        if (d == 1) {
-              //shift the shift register
-              for(j=1;j<=2*k;j++) {
-              	  buffer[:][j] = buffer[:][j-1];
-                  buffer[0:k-1][0] = tempbuffer;
-              }
+//store the first part of the line in a temporary buffer
+if (storage_index<k) {
+	tempbuffer[storage_index] = p_in.data;
+}
+// preparations outputting new line
+else if (storage_index == k) {
+		// reset pixel output index
+		output_index = 0;
+        for(j=1;j<=2*k;j++) {
+            //shift the shift register
+        	for(i=0;i<WIDTH-1;i++) {
+              	  buffer[i][j] = buffer[i][j-1];
+        	}
+        	// move temporary buffer to main buffer
+        	for(i=0;i<k-1;i++) {
+                  buffer[i][0] = tempbuffer[i];
+        	}
         }
-}
-else {
-    buffer[i][0] = p_in.data;
+        buffer[k][0] = p_in.data;
 }
 
-if ((lowerX = i-2*k) < 0){
+else {
+	// store incoming pixel
+    buffer[storage_index][0] = p_in.data;
+}
+
+// deal with nonexistent pixels to the left of the image
+if ((lowerX = output_index-k) < 0){
     lowerX = 0;
 }
-if ((upperX = i)> WIDTH-1){
+
+// deal with nonexistent pixels to the right of the image
+if ((upperX = output_index+k)> WIDTH-1){
     upperX = WIDTH-1;
 }
+//TODO: calculate the actual median
 //p_out.data = median(buffer[lowerX:upperX][]);
 
 
-i++;
+storage_index++;
+output_index++;
 if(p_in.last) {
-     d = k;
-     i = 0;
+     storage_index = 0;
 }
 
 // Write pixel to destination
